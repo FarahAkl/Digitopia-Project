685fec3eb2080618b5139fb4bb725b5e1984d47c
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-09-11 01:04:15 +0300

    refactor: clean up App component by removing unused code and improving structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,29 @@
-import { useEffect } from "react";
-// import { getAllReports } from "./services/report/apiGetAllReport";
-import { login } from "./services/Auth/apiLogin";
-import { register } from "./services/Auth/apiRegister";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { AuthProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Register from './pages/Register'
 
 function App() {
-  // 403 forbbiden
-  // useEffect(() => {
-  //   (async () => {
-  //     try {
-  //       const res = await getAllReports();
-  //       console.log("Reports response ✅:", res);
-  //     } catch (err) {
-  //       console.error("Reports error ❌:", err);
-  //     }
-  //   })();
-  // }, []);
-
-  useEffect(() => {
-    const testLogin = async () => {
-      try {
-        const data = await login({
-          email: "farahheshamakl@gmail.com",
-          password: "farah32l",
-        });
-        console.log("✅ Login success:", data);
-      } catch (error) {
-        console.error("❌ Login failed:", error);
-      }
-    };
-    testLogin();
-  }, []);
-
-  useEffect(() => {
-    const testRegister = async () => {
-      try {
-        const data = await register({
-          name: "Farah Akl",
-          email: "farah@example.com",
-          password: "Abcd1234!",
-          phoneNumber: "01012345678",
-          profileImageUrl: "https://example.com/profile.jpg",
-          location: "mansoura",
-        });
-        console.log("✅ Register success:", data);
-      } catch (error) {
-        console.error("❌ Register failed:", error);
-      }
-    };
-
-    testRegister();
-  }, []);
-
-  return <>hello</>;
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route path="/logout" element={<Logout />} /> */}
+          {/* <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          /> */}
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
