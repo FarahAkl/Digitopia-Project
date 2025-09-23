48a44b370ce870aab9550cd930d2a5e15f56c631
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-09-23 14:53:47 +0300

    fix: enhance ProtectedRoute to check for cookie token in addition to auth token

--- Frontend/src/ui/ProtectedRoute.tsx
+++ Frontend/src/ui/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router";
 import { useAuth } from "../hooks/useAuth";
+import { getCookie } from "../utils/TS-Cookie";
 
 type ProtectedRouteProps = {
   children: React.ReactNode;
@@ -8,7 +9,7 @@ type ProtectedRouteProps = {
 export default function ProtectedRoute({ children }: ProtectedRouteProps) {
   const auth = useAuth();
 
-  if (!auth?.token) {
+  if (!auth?.token || !getCookie({ name: "token" })) {
     return <Navigate to="/login" replace />;
   }
 
