32fb234fcca5c73b4a3efd6dec10a02693354cde
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-09-23 14:27:29 +0300

    fix: update Home link in Header component to point to the correct route

--- Frontend/src/ui/Header.tsx
+++ Frontend/src/ui/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="bg-primary flex h-[10vh] items-center justify-between px-5 py-4 text-amber-50 shadow-xl">
       <ul className="text-medium flex flex-1 items-center justify-evenly">
-        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/">Home</NavLink>
         <NavLink to="/map">Map</NavLink>
         <NavLink to="/contact">Contact</NavLink>
       </ul>
